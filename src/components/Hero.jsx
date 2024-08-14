284a2fb013403da31d3f7607e2e775a6b834027c
Yousef Saad <saadrslan95@gmail.com>
2024-08-14 22:39:05 +0100

    Hero Section added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,28 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div>
-      <h1> hero </h1>
+    <div className='min-h-screen flex flex-col gap-10
+    items-center justify-center 
+    text-center max-w-[900px] w-full mx-auto'>
+
+      <div className='flex flex-col gap-4'> 
+        <p>IT'S TIME TO GET</p>
+        <h1 className='uppercase font-semibold
+        text-4xl sm:text-5xl md:text-6xl 
+        lg:text-7xl'>Fit<span className='text-blue-400'>normous</span> </h1>
+      </div> 
+
+
+      <p className='text-sm md:text-base font-light'> I hereby acknowledgement that I may become
+         <span className='text-blue-400 font-medium'> unbelievably fitnormous </span>
+        and accept all risks of becoming the local
+        <span className='text-blue-400 font-medium'>mass monstrosity</span>, afflicted with severe body dismorphia, unable to fit through doors. 
+      </p>
+
+      <button className='px-8 py-4 rounded-3xl'>
+        <p>Accept & Begin</p>
+      </button>
+
     </div>
   )
 }
