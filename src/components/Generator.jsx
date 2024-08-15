9de2e80f5fd284d44d4c13bf8d214e0b306924bc
Yousef Saad <saadrslan95@gmail.com>
2024-08-15 19:59:21 +0100

    Buttons in generator section added

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import SectionWrapper from './SectionWrapper'
 import Header from './Header'
+import { WORKOUTS } from '../utils/swoldier'
 
 
 export default function Generator() {
@@ -9,8 +10,22 @@ export default function Generator() {
       header = {"generate your workout"} 
       title = {["It\s", "Huge", "o\ clock"]}
      >
-      
+
     <Header index={'01'} title={'Pick Your poison'}description={'Select the workout you want to enjoy.'}/>
+
+    <div className='grid grid-cols-2 sm:grid-cols-4 gap-4'>
+
+      {Object.keys(WORKOUTS).map((type, typeIndex)=> {
+        return (
+          <button key={typeIndex} className=' blueShadow
+          bg-slate-950 border py-3 border-blue-400 rounded-3xl'>
+              <p className='capitalize'>{type.replaceAll("_"," ")}</p>
+          </button>
+        )
+        
+      })}
+    </div>
+
     </SectionWrapper>
   
   )
