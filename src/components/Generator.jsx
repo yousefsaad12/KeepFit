2c250c517175c85b2b8193c2e9fa5134e85e1cae
Yousef Saad <saadrslan95@gmail.com>
2024-08-15 18:13:01 +0100

    Header component added but in the same file with generator

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import SectionWrapper from './SectionWrapper'
 
+function Header(props)
+{ 
+  const {index, title, description} = props
+  return (
+    <div>
+      <div>
+        <p>{index}</p>
+        <h4>{title}</h4>
+      </div>
+      <p>{description}</p>
+    </div>
+  )
+}
 export default function Generator() {
   return (
     <SectionWrapper 
       header = {"generate your workout"} 
       title = {["It\s", "Huge", "o\ clock"]}
-
     >
+
+    <Header index={'01'} title = {'Pick Your poison'}
+    description={"Select the workout you want to enjoy."}/>
 
     </SectionWrapper>
   )
