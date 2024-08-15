a291147f722c50b06888959304e0a000e6a8c9f4
Yousef Saad <saadrslan95@gmail.com>
2024-08-15 19:47:58 +0100

    Header added with its styling and section wrapper also

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -5,16 +5,13 @@ import Header from './Header'
 
 export default function Generator() {
   return (
-    <>
      <SectionWrapper 
       header = {"generate your workout"} 
       title = {["It\s", "Huge", "o\ clock"]}
-    >
-       
+     >
+      
+    <Header index={'01'} title={'Pick Your poison'}description={'Select the workout you want to enjoy.'}/>
     </SectionWrapper>
-    <Header index={'01'} title={'Pick Your poison'}
-    description={'Select the workout you want to enjoy.'}/>
   
-    </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 export default function Header(props)
 { 
-  const {index, title, description} = props
-  return (
-    <div>
-      <div>
-        <p>{index}</p>
-        <h4>{title}</h4>
-      </div>
-      <p>{description}</p>
-    </div>
-  )
+        const { index, title, description } = props
+        return (
+            <div className='flex flex-col gap-4'>
+                <div className='flex items-center justify-center gap-2'>
+                    <p className='text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400'>{index}</p>
+                    <h4 className='text-xl sm:text-2xl md:text-3xl'>{title}</h4>
+                </div>
+                <p className='text-sm sm:text-base mx-auto'>{description}</p>
+            </div>
+        )
+    
 }
