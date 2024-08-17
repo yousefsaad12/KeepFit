83092625e65364c66b29cd4bfbfb6b80732951a6
Yousef Saad <saadrslan95@gmail.com>
2024-08-17 23:32:11 +0100

    use state added to generator

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SectionWrapper from './SectionWrapper'
 import Header from './Header'
 import { WORKOUTS } from '../utils/swoldier'
 
 
 export default function Generator() {
+
+  const [showModel, setshowModel] = useState(false)
+
+  function toggleModel()
+  {
+    setshowModel(!showModel)
+  }
+  
   return (
      <SectionWrapper 
       header = {"generate your workout"} 
       title = {["It\s", "Huge", "o\ clock"]}
      >
 
-    <Header index={'01'} title={'Pick Your poison'}description={'Select the workout you want to enjoy.'}/>
+    <Header index={'01'} title={'Pick Your Poison'}description={'Select the workout you want to enjoy.'}/>
 
     <div className='grid grid-cols-2 sm:grid-cols-4 gap-4'>
 
@@ -24,6 +32,21 @@ export default function Generator() {
         )
         
       })}
+    </div>
+
+    <Header index={'02'} title={'Lock On Targets'}description={'Select the muscles judged for annihilation.'}/>
+
+    <div className='bg-slate-950  border border-solid 
+      border-blue-400 rounded-lg flex flex-col'>
+    <button onClick={toggleModel}
+     className='relative flex p-3 items-center justify-center'>
+        <p>Select muscle groups</p>
+        <i className='fa-solid 
+        absolute right-3 top-1/2 -translate-y-1/2 fa-caret-down'></i>
+      </button>
+      {showModel && (
+        <div>model</div>
+      )}
     </div>
 
     </SectionWrapper>
